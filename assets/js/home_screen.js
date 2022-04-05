498831c83c3ae47ba0246822f571e68ff0ce7120
shinyZu <shinysirichandra1141@gmail.com>
2022-04-06 05:05:45 +0530

    Updated content

--- assets/js/home_screen.js
+++ assets/js/home_screen.js
@@ -8,8 +8,10 @@ $("#help").css("display", "none");
 $("#sound-icon").click(function (e) {
     if (!audio1.paused) {
         audio1.pause();
+        $("div#controller-icons>button#sound-icon").addClass("pressed");
     } else {
         audio1.play();
+        $("div#controller-icons>button#sound-icon").removeClass("pressed");
     }
 });
 
@@ -20,10 +22,12 @@ $("#btnPlay").click(function (e) {
 
 $("#help-icon").click(function (e) {
     $("#help").css("display", "block");
-
+    $("div#controller-icons>button#help-icon").addClass("pressed");
+    $("div#controller-icons>button#help-icon").css("pointer-events", "none");
 });
 
 $("#btnClose").click(function (e) {
     $("#help").css("display", "none");
-
+    $("div#controller-icons>button#help-icon").removeClass("pressed");
+    $("div#controller-icons>button#help-icon").css("pointer-events", "auto");
 });
